52150c5919222bb014060d917f36fd0b8d6e417f
Jake Brukhman <jbrukh@gmail.com>
2014-08-03 15:51:50 -0400

    Save.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,17 +3,19 @@ require 'hashids'
 class StaticPagesController < ApplicationController
 
   @@hashids = Hashids.new('salt', 8)
-  
+
+  # index  
   def index
-  	@ref = params[:ref]
-  	if @ref 
-  		@user = User.find @@hashids.decrypt(@ref)
+  	ref = params[:ref]
+  	if ref 
+  		@ref_user = User.find @@hashids.decrypt(ref)
   	end
+  	@id = session[:user_id]
+  	@ref_code = @@hashids.encrypt(@id) if @id
   end
 
   def thanks
   	@obf_id = @@hashids.encrypt(session[:user_id])
   end
 	
-
 end
